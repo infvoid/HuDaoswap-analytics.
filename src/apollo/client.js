29dfb185f383cd094913273cf271bf62bf6c7663
infvoid <infvoid.defi@yandex.ru>
2021-06-09 15:17:02 +0800

    init commit

---
+++ src/apollo/client.js
@@ -0,0 +1,34 @@
+import { ApolloClient } from 'apollo-client'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { HttpLink } from 'apollo-link-http'
+
+export const client = new ApolloClient({
+  link: new HttpLink({
+    uri: 'https://q.hg.network/subgraphs/name/hubswap/heco',
+  }),
+  cache: new InMemoryCache(),
+  shouldBatch: true,
+})
+
+export const healthClient = new ApolloClient({
+  link: new HttpLink({
+    uri: 'https://h.hg.network/graphql',
+  }),
+  cache: new InMemoryCache(),
+  shouldBatch: true,
+})
+
+export const stakingClient = new ApolloClient({
+  link: new HttpLink({
+    uri: 'https://api.thegraph.com/subgraphs/name/way2rach/talisman',
+  }),
+  cache: new InMemoryCache(),
+  shouldBatch: true,
+})
+
+export const blockClient = new ApolloClient({
+  link: new HttpLink({
+    uri: 'https://q.hg.network/subgraphs/name/hecoblocks/heco',
+  }),
+  cache: new InMemoryCache(),
+})
