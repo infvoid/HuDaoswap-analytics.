c6795691d323055ff061edb00c3af20931a9e394
infvoid <infvoid.defi@yandex.ru>
2021-06-13 20:56:00 +0800

    fix token logo

--- src/components/TokenLogo/index.js
+++ src/components/TokenLogo/index.js
@@ -71,9 +71,9 @@ export default function TokenLogo({ address, header = false, size = '24px', ...r
     )
   }
 
-  const path = `https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/${isAddress(
+  const path = `/coin/${isAddress(
     address
-  )}/logo.png`
+  )}.png`
 
   return (
     <Inline>
